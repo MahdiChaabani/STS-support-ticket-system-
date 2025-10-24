961c6eafe3253ebc8f8cd01b4d5ad00651d18058
rednt <47644820+rednt@users.noreply.github.com>
2025-10-24 13:16:50 +0100

    move AuthForm and Landing components to Pages directory

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,8 @@
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React, { useEffect, useRef } from 'react';
-import Landing from "./components/Landing";
-import AuthForm from "./components/AuthForm";
+import Landing from "./Pages/Landing";
+import AuthForm from "./Pages/AuthForm";
 import Lenis from 'lenis';
 
 function App() {
